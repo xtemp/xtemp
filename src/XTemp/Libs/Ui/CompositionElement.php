267a6fcbee553a75636b87363bcaa8aeba4a0249
Radek Burget <burgetr@fit.vutbr.cz>
2014-06-17 10:43:14 +0200

    Optional template attribute for compositions

--- src/XTemp/Libs/Ui/CompositionElement.php
+++ src/XTemp/Libs/Ui/CompositionElement.php
@@ -17,30 +17,43 @@ class CompositionElement extends CompoundElementBase
 	public function __construct($domElement)
 	{
 		parent::__construct($domElement);
+		$this->template = $this->useAttr('template', null);
+	}
+	
+	/**
+	 * Sets the nested composition. The nested composition does not load the template by itself; the contents
+	 * of the ui:insert elements is obtained from outside.
+	 * @param unknown $nested
+	 */
+	public function setNested($nested)
+	{
+		$this->nested = $nested;
 	}
 	
 	public function restructureTree()
 	{
-		$this->template = $this->requireAttr('template');
-		$ttree = $this->createTemplateTree($this->template);
-		$troot = $ttree->getRoot();
-		
-		//find the ui:define tags and match them
-		foreach ($this->getChildren() as $child)
+		if ($this->template)
 		{
-			if ($child instanceof DefineElement)
+			$ttree = $this->createTemplateTree($this->template);
+			$troot = $ttree->getRoot();
+			
+			//find the ui:define tags and match them
+			foreach ($this->getChildren() as $child)
 			{
-				$name = $child->getName();
-				$ins = $this->findInsert($name, $troot);
-				if ($ins)
+				if ($child instanceof DefineElement)
 				{
-					$ins->removeAllChildren();
-					$ins->addAll($child->getChildren());
+					$name = $child->getName();
+					$ins = $this->findInsert($name, $troot);
+					if ($ins)
+					{
+						$ins->removeAllChildren();
+						$ins->addAll($child->getChildren());
+					}
 				}
 			}
+			
+			$this->getTree()->setRoot($troot);
 		}
-		
-		$this->getTree()->setRoot($troot);
 	}
 	
 	public function render()
