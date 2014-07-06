6641c8bc1bc9f21e16932cb69a8b48c0b2dcbdd9
Radek Burget <burgetr@fit.vutbr.cz>
2014-07-06 23:21:57 +0200

    Simple c:component element

---
+++ src/XTemp/Libs/Core/ComponentElement.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * XTemp - XML Templating Engine for PHP
+ * ComponentElement.php created on 6. 7. 2014 by burgetr
+ */
+
+namespace XTemp\Libs\Core;
+
+/**
+ *
+ * @author      burgetr
+ */
+class ComponentElement extends \XTemp\Tree\Element
+{
+	private $name;
+	
+	public function __construct($domElement)
+	{
+		parent::__construct($domElement);
+		$this->name = $this->requireAttrExpr('name');
+	}
+	
+	public function render()
+	{
+		return '{control ' . $this->name . '}';
+	}
+
+}
