bee6b51e3773a89ccf9547504b5b448658a2b98b
Radek Burget <burgetr@fit.vutbr.cz>
2014-07-29 22:57:58 +0200

    SessionScoped properties in presenters

--- src/XTemp/Bridges/Nette/XTempPresenter.php
+++ src/XTemp/Bridges/Nette/XTempPresenter.php
@@ -18,6 +18,13 @@ class XTempPresenter extends \Nette\Application\UI\Presenter
 	{
 		parent::startup();
 		$this->template->getLatte()->setLoader(new \XTemp\Loader($this));
+		$this->restoreSessionProperties();
+	}
+	
+	public function shutdown($response)
+	{
+		parent::shutdown($response);
+		$this->saveSessionProperties();
 	}
 	
 	public function formatTemplateFiles()
@@ -30,6 +37,43 @@ class XTempPresenter extends \Nette\Application\UI\Presenter
 				"$dir/templates/$presenter/$this->view.xhtml",
 				"$dir/templates/$presenter.$this->view.xhtml",
 		);
+	}
+
+	//===========================================================================
+	
+	public function getSessionProperties()
+	{
+		$rc = new \Nette\Reflection\ClassType(get_called_class());
+		$params = array();
+		foreach ($rc->getProperties() as $rp) {
+			if (!$rp->isStatic() && $rp->hasAnnotation('SessionScoped')) {
+				$params[] = $rp->getName();
+			}
+		}
+		return $params;
+	}
+	
+	protected function saveSessionProperties()
+	{
+		$props = array();
+		foreach ($this->getSessionProperties() as $prop)
+		{
+			//echo "store: $prop = " . $this->$prop . "<br>";
+			$props[$prop] = $this->$prop;
+		}
+		$session = $this->getSession('XTemp/SessionScope');
+		$session->properties = $props;
+	}
+	
+	protected function restoreSessionProperties()
+	{
+		$session = $this->getSession('XTemp/SessionScope');
+		$props = $session->properties;
+		foreach ($this->getSessionProperties() as $prop)
+		{
+			if (isset($props[$prop]))
+				$this->$prop = $props[$prop];
+		}
 	}
 	
 }
