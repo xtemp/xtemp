ec87d79a25c646a115ed17e75e557d5e88350614
Radek Burget <burgetr@fit.vutbr.cz>
2014-08-05 23:12:48 +0200

    More input fields

---
+++ src/XTemp/Libs/Html/InputSecretElement.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * XTemp - XML Templating Engine for PHP
+ * InputSecretElement.php created on 5. 8. 2014 by burgetr
+ */
+
+namespace XTemp\Libs\Html;
+
+/**
+ *
+ * @author      burgetr
+ */
+class InputSecretElement extends InputField
+{
+	private $required;
+	private $requiredMessage;
+	
+	protected function loadParams()
+	{
+		parent::loadParams();
+		$this->required = $this->useAttrPlain("required", "false", array("true", "false"));
+		$this->requiredMessage = $this->useAttrExpr("requiredMessage", 'Value required');
+	}
+
+	public function beforeRender()
+	{
+		parent::beforeRender();
+	}
+	
+	public function render()
+	{
+		return '{input ' . $this->id . '}';
+	}
+	
+	public function getFnCall()
+	{
+		$lbl = '$labels[' . $this->id . ']';
+		$ret = 'addPassword(' . $this->id . ", isset($lbl)?$lbl:'')";
+		//$ret .= '->setValue(' . $this->value->toPHP() . ')';
+		if ($this->required == "true")
+		{
+			$ret .= '->setRequired(' . $this->requiredMessage->toPHP() . ')';
+		}
+		return $ret;
+	}
+}
