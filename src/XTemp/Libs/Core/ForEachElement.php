fbf8fad1390bc87195da038ed6cd13686bda171e
Radek Burget <burgetr@fit.vutbr.cz>
2014-08-26 14:40:00 +0200

    Context variable mapping in c:forEach

--- src/XTemp/Libs/Core/ForEachElement.php
+++ src/XTemp/Libs/Core/ForEachElement.php
@@ -12,18 +12,20 @@ namespace XTemp\Libs\Core;
  */
 class ForEachElement extends \XTemp\Tree\Element
 {
-	private $items;
-	private $var;
+	protected static $keyindex = 1;
+	
+	private $value;
+	private $varname;
 	private $varStatus;
 	
 	protected function loadParams()
 	{
-		$this->items = $this->requireAttrExpr('items');
-		$this->var = $this->requireAttrVar('var');
+		$this->value = $this->requireAttrExpr('items');
+		$this->varname = $this->requireAttrVar('var');
 		$this->varStatus = $this->useAttrVar('varStatus', NULL);
 	}
 	
-	public function render()
+	public function rrender()
 	{
 		$ret = "\n{foreach {$this->items->toPHP()} as {$this->var}}\n";
 		if ($this->varStatus !== NULL && $this->varStatus != '$iterator')
@@ -32,5 +34,48 @@ class ForEachElement extends \XTemp\Tree\Element
 		$ret .= "\n{/foreach}\n";
 		return $ret;
 	}
+
+	public function render()
+	{
+		$ret = '';
+	
+		$doMapping = $this->value->isLValue();
+		$ret .= $this->renderIterationStart($doMapping);
+		$ret .= $this->renderChildren();
+		$ret .= $this->renderIterationEnd($doMapping);
+		return $ret;
+	}
+	
+	protected function renderIterationStart($doMapping)
+	{
+		$ret = '';
+		if ($doMapping)
+		{
+			$keyname = '$_xt_fe_key' . (self::$keyindex++);
+			$ctrlname = '$_xt_fe_ctrl' . (self::$keyindex++);
+			$mapping = $this->value->getLValueIdString() . "[$keyname]";
+				
+			$ret .= '{var ' . $ctrlname . '=' . $this->value->toPHP() . '}';
+			$ret .= '{foreach ' . $ctrlname . ' as ' . $keyname . '=>' . $this->varname . "}\n";
+			$ret .= '{? $_xt_ctx->open("' . $mapping . '", array(\'' . substr($this->varname, 1) . '\'=>' . $ctrlname . '[' . $keyname . ']))}';
+		}
+		else
+		{
+			$ret .= '{foreach ' . $this->value->toPHP() . ' as ' . $this->varname . "}\n";
+		}
+		if ($this->varStatus !== NULL && $this->varStatus != '$iterator')
+			$ret .= "{var {$this->varStatus}=\$iterator}";
+		return $ret;
+	}
+	
+	protected function renderIterationEnd($doMapping)
+	{
+		$ret = '';
+		if ($doMapping)
+			$ret .= '{? $_xt_ctx->close()}';
+		$ret .= "{/foreach}\n";
+		return $ret;
+	}
+	
 	
 }
