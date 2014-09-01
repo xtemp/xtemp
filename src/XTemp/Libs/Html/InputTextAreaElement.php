7114abc6bbc656dcbfd1148238fca0f8ada08c48
Radek Burget <burgetr@fit.vutbr.cz>
2014-09-01 21:26:18 +0200

    h:inputTextArea

---
+++ src/XTemp/Libs/Html/InputTextAreaElement.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * XTemp - XML Templating Engine for PHP
+ * InputTextAreaElement.php created on 2. 9. 2014 by burgetr
+ */
+
+namespace XTemp\Libs\Html;
+
+/**
+ *
+ * @author      burgetr
+ */
+class InputTextAreaElement extends InputField
+{
+	
+	public static function addToForm($form, $name, $label, $value, $params)
+	{
+		$f = $form->addTextArea($name, $label);
+		if ($value !== NULL)
+			$f->setValue($value);
+		if (isset($params['required']) && $params['required'] === TRUE)
+			$f->setRequired($params['requiredMessage']);
+	}
+
+}
